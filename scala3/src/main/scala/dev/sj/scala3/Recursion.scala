440cbc01e21ac0eec73fab7eed1c4fbb03b94d87
0xsj <sjtommylee@gmail.com>
2024-08-26 17:00:46 -0700

    recurse part 2

--- scala3/src/main/scala/dev/sj/scala3/Recursion.scala
+++ scala3/src/main/scala/dev/sj/scala3/Recursion.scala
@@ -1,5 +1,18 @@
 package dev.sj.scala3
 
-object Recursion {
+object Recursion extends App:
+  def factorial(n: Int): Int =
+    if (n <=1) 1
+    else n * factorial(n -1)
 
-}
+  println(factorial(10))
+
+
+  def anotherFactorial(n: Int): Int =
+    def factorialHelper(x: Int, accumulator: Int): Int =
+      if (x <= 1) accumulator
+      else factorialHelper(x - 1, x * accumulator)
+    factorialHelper(n, 1)
+
+
+  println(anotherFactorial(5000))
